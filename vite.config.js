e9867e92d38e05157605ec334e619bdc59e547d8
ondialogue <86033796+ondialogue@users.noreply.github.com>
2026-04-30 13:13:18 +0800

    update path for let-your-life-speak

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // 引入插件
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // 将 tailwind 添加到这里
+  ],
   base: '/let-your-life-speak/',
 })
